f9745534e2cf1eb5cc7a1ed4556257eedcfaf980
gurusharangupta <gupta.gurusharan@gmail.com>
2019-06-11 14:16:46 +0530

    1 file modified, 12 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, RecipeListComponent, RecipeDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/recipe/recipe.model.ts
@@ -0,0 +1,6 @@
+export class Recipe{
+
+  name: string;
+  type: string;
+  
+}
