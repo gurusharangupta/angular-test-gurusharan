53f7001b9a159e4b62ef3255d23cd950e0433ebc
gurusharangupta <gupta.gurusharan@gmail.com>
2019-06-11 18:27:03 +0530

    3 files modified

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -7,7 +7,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [new Recipe('Pizza', 'Italian', 'https://i2.wp.com/www.staceyhomemaker.com/wp-content/uploads/2019/01/Vegan-Keto-Recipes-.jpg?resize=683%2C1024&ssl=1')];
+  recipes: Recipe[] = [new Recipe('Pizza', 'Italian', 'https://i2.wp.com/www.staceyhomemaker.com/wp-content/uploads/2019/01/Vegan-Keto-Recipes-.jpg?resize=683%2C1024&ssl=1'),
+  new Recipe('Fries', 'Belgian', 'https://i2.wp.com/www.staceyhomemaker.com/wp-content/uploads/2019/01/Vegan-Keto-Recipes-.jpg?resize=683%2C1024&ssl=1')];
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   constructor() { }
 
