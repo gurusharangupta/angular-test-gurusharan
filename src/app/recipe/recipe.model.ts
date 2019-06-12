aa4d5f6095a7aaa371e34797c1870308b30bd825
gurusharangupta <gupta.gurusharan@gmail.com>
2019-06-13 00:29:13 +0530

    2 files modified

--- src/app/recipe/recipe.model.ts
+++ src/app/recipe/recipe.model.ts
@@ -1,11 +1,21 @@
 import { Ingredient } from '../shared/ingredient.model';
 export class Recipe {
 
+  name: string;
+  description: string;
+  imagePath: string;
+  ingredient: Ingredient[];
+
   public constructor(
-    public name: string,
-    public description: string,
-    public imagePath: string,
-    public ingredient: Ingredient[]) { }
+    name: string,
+    description: string,
+    imagePath: string,
+    ingredient: Ingredient[]) { 
+      this.name = name;
+      this.description = description;
+      this.imagePath = imagePath;
+      this.ingredient = ingredient;
+    }
 
 
 }
