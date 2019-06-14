8c5b72d0d643ae6fdd8a357637cd2c029d88a67d
gurusharangupta <gupta.gurusharan@gmail.com>
2019-06-14 18:09:10 +0530

    3 files modified

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import { RecipeService } from './../recipe.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
@@ -9,9 +10,16 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
 
 @Input() recipe: Recipe;
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private routes: ActivatedRoute) { }
 
   ngOnInit() {
+     this.recipe = this.recipeService.getRecipeFromName(this.routes.snapshot.params['name']);
+     this.routes.params.subscribe(
+       (recipe: Recipe) => {
+         this.recipe = recipe;
+       }
+     );
+   
   }
 
   addIngredientsToShopping(){
--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -6,31 +6,39 @@ import { Ingredient } from '../shared/ingredient.model';
 @Injectable()
 export class RecipeService {
 
-public recipeSelected = new EventEmitter<Recipe>()
-private recipes: Recipe[] = [new Recipe(
-  'Pizza',
-   'Italian',
+  public recipeSelected = new EventEmitter<Recipe>()
+  private recipes: Recipe[] = [new Recipe(
+    'Pizza',
+    'Italian',
     'https://i2.wp.com/www.staceyhomemaker.com/wp-content/uploads/2019/01/Vegan-Keto-Recipes-.jpg?resize=683%2C1024&ssl=1',
     [
-      new Ingredient('Toppings',1),
-       new Ingredient('Base',1)
-      ]
-    ),
+      new Ingredient('Toppings', 1),
+      new Ingredient('Base', 1)
+    ]
+  ),
   new Recipe(
-    'Lasagna', 
+    'Lasagna',
     'Belgian',
-     'https://i2.wp.com/www.staceyhomemaker.com/wp-content/uploads/2019/01/Vegan-Keto-Recipes-.jpg?resize=683%2C1024&ssl=1',
-     [
-      new Ingredient('Cheese',1),
-       new Ingredient('Fries',20)
-      ])];
+    'https://i2.wp.com/www.staceyhomemaker.com/wp-content/uploads/2019/01/Vegan-Keto-Recipes-.jpg?resize=683%2C1024&ssl=1',
+    [
+      new Ingredient('Cheese', 1),
+      new Ingredient('Fries', 20)
+    ])];
   constructor(private shoppingListService: ShoppingListService) { }
 
-public getRecipes(){
-  return this.recipes.slice();
-}
+  public getRecipes() {
+    return this.recipes.slice();
+  }
 
-public addIngredientsForShopping(ingredient: Ingredient[]){
-this.shoppingListService.addIngredients(ingredient);
-}
+  public addIngredientsForShopping(ingredient: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredient);
+  }
+
+  public getRecipeFromName(name: string){
+    for(let recipe of this.recipes){
+      if(name == recipe.name){
+        return recipe;
+      }
+    }
+  }
 }
