95e53bc104cc231ae97e0c4b41c7e1c50f599d10
gurusharangupta <gupta.gurusharan@gmail.com>
2019-06-18 17:33:01 +0530

    2 files modified

--- src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
@@ -14,7 +14,7 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
   recipeForm: FormGroup;
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService,private router: Router) { }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -37,8 +37,10 @@ export class RecipeEditComponent implements OnInit {
 
     if (this.editMode) {
       this.recipeService.updateRecipe(this.id, recipe);
+      this.router.navigate(['../'],{relativeTo: this.route});
     } else {
       this.recipeService.addRecipe(recipe);
+      this.router.navigate(['../'],{relativeTo: this.route});
     }
 
   }
