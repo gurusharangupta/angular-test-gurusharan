dfaed262905d5224a2759702116b1af7640cf46c
gurusharangupta <gupta.gurusharan@gmail.com>
2019-06-24 17:27:59 +0530

    2 files modified

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 interface AuthResponseData{
   kind:string;
@@ -15,6 +15,14 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   signUp(email: string, password: string) {
+    const httpOptions = {
+  headers: new HttpHeaders({ 
+    'Access-Control-Allow-Origin': '*',
+     'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+     'Content-Type': 'application/json'
+    
+  })
+};
     return this.http.post<AuthResponseData>('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBUhRZwz4CVDoBjYfw-ldzCILvu1rn-PDI',
     {
       email: email,
