c48eb7d734a418109732bfb8a722816e4da8983f
gurusharangupta <gupta.gurusharan@gmail.com>
2019-06-24 14:19:07 +0530

    2 files modified

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  signForm(email: string, password: string) {
+    this.http.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBUhRZwz4CVDoBjYfw-ldzCILvu1rn-PDI',
+    {
+      email: email,
+      password: password,
+      
+    })
+  }
 }
