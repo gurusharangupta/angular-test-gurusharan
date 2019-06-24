6da43c97076b617822e6424c02ecd24c7ea114f4
gurusharangupta <gupta.gurusharan@gmail.com>
2019-06-24 14:39:09 +0530

    Added Header for signUP

--- src/app/shared/auth-interceptor.service.ts
+++ src/app/shared/auth-interceptor.service.ts
@@ -1,12 +1,18 @@
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 
 
 export class AuthInterceptorService implements HttpInterceptor {
 
+ 
+ 
   intercept(req: HttpRequest<any>, next: HttpHandler){
       console.log('request is on its way');
-      const modifiedRequest = req.clone({ headers: req.headers.append('Auth','xyz')})
+       letheader =  new HttpHeaders({
+     'Access-Control-Allow-Origin': '*',
+     'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+     'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token' })
+      const modifiedRequest = req.clone({ headers: header})
       return next.handle(modifiedRequest).pipe(
         tap(events => {
           console.log('modified response');
