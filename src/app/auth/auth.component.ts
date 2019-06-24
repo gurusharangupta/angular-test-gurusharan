cc3b6e1e6e2e688c2a8359569bae802cfdda7b6e
gurusharangupta <gupta.gurusharan@gmail.com>
2019-06-24 14:33:36 +0530

    2 files modified

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService }from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -7,18 +8,35 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
- isLoginMode = false;
-  constructor() { }
+  isLoginMode = false;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
-  switchMode(){
+  switchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
-  onSubmit(form: NgForm){
- console.log(form.value);
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    if(!form.valid){
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    if(this.isLoginMode){
+
+    }else{
+      this.authService.signUp(email,password).subscribe(
+        resData => {
+          console.log(resData);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
   }
 
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface AuthResponseData{
+  kind:string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
 @Injectable()
 export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  signForm(email: string, password: string) {
-    this.http.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBUhRZwz4CVDoBjYfw-ldzCILvu1rn-PDI',
+  signUp(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBUhRZwz4CVDoBjYfw-ldzCILvu1rn-PDI',
     {
       email: email,
       password: password,
-      
+      returnSecureToken: true
+
     })
   }
 }
